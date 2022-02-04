6d8536f35b20e2affa5c4f3fec6d89e384890f1d
jesusparra94 <jdparrau@gmail.com>
2022-02-04 18:37:22 -0300

    dominios componente

--- src/app/configuracion/pages/dominio/dominio.component.ts
+++ src/app/configuracion/pages/dominio/dominio.component.ts
@@ -1,8 +1,9 @@
+import { Productos } from './../../../ecommerce/interfaces/ecommerce.interface';
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { DominiosService } from '../../../ecommerce/services/dominios.service';
 import { Result, PrecioDominios } from '../../../ecommerce/interfaces/dominios.interfaces';
 import {  FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { TotalCarro } from '../../../ecommerce/interfaces/ecommerce.interface';
+import { TotalCarro, Carrito } from '../../../ecommerce/interfaces/ecommerce.interface';
 
 @Component({
   selector: 'app-dominio',
@@ -10,13 +11,20 @@ import { TotalCarro } from '../../../ecommerce/interfaces/ecommerce.interface';
 })
 export class DominioComponent implements OnInit {
 
+  
   mostrar:number = 0;
   mostrarold: number = 0;
   dominios: Result[] = [];
   preciodominio: PrecioDominios[] = [];
   dominiobuscado:string = '';
+  dominioguardado:string = '';
 
   form:FormGroup = this.fb.group({
+    dominio:['',Validators.required],
+    extension:['',Validators.required]
+  })
+
+  form2:FormGroup = this.fb.group({
     dominio:['',Validators.required],
     extension:['',Validators.required]
   })
@@ -28,11 +36,36 @@ export class DominioComponent implements OnInit {
 
   @Output() totalcarrod: EventEmitter<TotalCarro> = new EventEmitter();
   @Input() dominionum!:number;
+  @Input() Dominioscarrito:Carrito[] = [];
+
 
   constructor(private DominiosService:DominiosService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    
+  }
 
+  limpiarDomGuardado(){
+    this.dominioguardado = '';
+  }
+  guardardominio(){
+    if(this.form2.invalid){
+      this.form2.markAllAsTouched()
+      return;
+    }
+
+    const dominio = this.form2.value.dominio;
+    const extension = this.form2.value.extension;
+
+    this.dominioguardado = `${dominio}${extension}`;
+
+    let index = JSON.parse(localStorage.getItem('index')!);
+    let carrito: Carrito[] =  JSON.parse(localStorage.getItem('carrito')!);
+    carrito[index].dominio = this.dominioguardado;
+    localStorage.setItem('carrito',JSON.stringify(carrito));
+
+    //console.log(JSON.parse(localStorage.getItem('carrito')!));
+    
   }
 
   buscardominio(){
@@ -61,9 +94,6 @@ export class DominioComponent implements OnInit {
 
   }
 
-
-
-
   nuevodominio(){
     console.log(this.mostrar)
 
@@ -74,6 +104,8 @@ export class DominioComponent implements OnInit {
       this.mostrar = 0;
       this.mostrarold = 0;
     }
+
+    this.dominioguardado = '';
 
   }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  urlBase: 'http://backendcreattiva.cp/api',
-  //  urlBase: 'https://creattiva-api.cl/api',
+  //urlBase: 'http://backendcreattiva.cp/api',
+    urlBase: 'https://creattiva-api.cl/api',
 
 };
 
